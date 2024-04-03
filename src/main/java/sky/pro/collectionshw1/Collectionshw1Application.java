dfc4dbf91756eee2899a0cb3758f43eefb8efbc9
Андрей <79538233369@ya.ru>
2024-04-03 14:58:51 +0500

    Initial commit

---
+++ src/main/java/sky/pro/collectionshw1/Collectionshw1Application.java
@@ -0,0 +1,13 @@
+package sky.pro.collectionshw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collectionshw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Collectionshw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/collectionshw1/Collectionshw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.collectionshw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collectionshw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
